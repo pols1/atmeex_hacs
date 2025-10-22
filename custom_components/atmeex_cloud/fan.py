5e9e371df46417e67832a684dd28386b2fe0c73a
pols <75303516+pols1@users.noreply.github.com>
2025-10-22 15:16:43 +0300

    Update fan.py

--- custom_components/atmeex_cloud/fan.py
+++ custom_components/atmeex_cloud/fan.py
@@ -1,6 +1,6 @@
 from homeassistant.config_entries import ConfigEntry
-
 from .const import DOMAIN
 
 async def async_setup_entry(hass, config_entry: ConfigEntry, async_add_entities):
-    coordinator = hass.data[DOMAIN][config_entry.entry_id]
+    # Пока без fan-сущностей; добавим после уточнения API.
+    return
