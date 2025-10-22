bb94ec61c50f6d5bae58f37baf51655893bcc194
pols <75303516+pols1@users.noreply.github.com>
2025-10-22 15:41:22 +0300

    Update config_flow.py

--- custom_components/atmeex_cloud/config_flow.py
+++ custom_components/atmeex_cloud/config_flow.py
@@ -1,43 +1,27 @@
-from __future__ import annotations
-import logging
+from homeassistant import config_entries
 import voluptuous as vol
-
-from homeassistant.config_entries import ConfigFlow
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
-
+from .api import AtmeexApi, ApiError
 from .const import DOMAIN
-from .api import AtmeexApi
 
-_LOGGER = logging.getLogger(__name__)
+DATA_SCHEMA = vol.Schema({
+    vol.Required(CONF_EMAIL): str,
+    vol.Required(CONF_PASSWORD): str,
+})
 
-STEP_USER_DATA_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_EMAIL): str,
-        vol.Required(CONF_PASSWORD): str,
-    }
-)
-
-class AtmeexConfigFlow(ConfigFlow, domain=DOMAIN):
+class AtmeexConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         errors = {}
-        if user_input is not None:
-            api = AtmeexApi("https://api.iot.atmeex.com")
+        if user_input:
+            api = AtmeexApi()
             await api.async_init()
             try:
                 await api.login(user_input[CONF_EMAIL], user_input[CONF_PASSWORD])
-                # sanity-check запросим список устройств
                 await api.get_devices()
-            except Exception as exc:
-                _LOGGER.exception("Login/devices failed")
+            except ApiError:
                 errors["base"] = "cannot_connect"
             else:
-                return self.async_create_entry(
-                    title=user_input.get(CONF_EMAIL),
-                    data=user_input,
-                )
-            finally:
-                await api.async_close()
-
-        return self.async_show_form(step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors)
+                return self.async_create_entry(title=user_input[CONF_EMAIL], data=user_input)
+        return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA, errors=errors)
