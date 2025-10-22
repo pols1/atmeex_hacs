f7cae1f4930eba13494dd2d568cf13bed71e8bd4
pols <75303516+pols1@users.noreply.github.com>
2025-10-22 15:42:28 +0300

    Update fan.py

--- custom_components/atmeex_cloud/fan.py
+++ custom_components/atmeex_cloud/fan.py
@@ -1,6 +1,25 @@
-from homeassistant.config_entries import ConfigEntry
-from .const import DOMAIN
+from homeassistant.components.fan import FanEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-async def async_setup_entry(hass, config_entry: ConfigEntry, async_add_entities):
-    # Пока без fan-сущностей; добавим после уточнения API.
-    return
+class AtmeexFan(CoordinatorEntity, FanEntity):
+    def __init__(self, coordinator, api, device):
+        super().__init__(coordinator)
+        self.api = api
+        self._device_id = device["id"]
+        self._attr_name = device["name"]
+
+    @property
+    def condition(self):
+        return self.coordinator.data["states"].get(str(self._device_id), {})
+
+    @property
+    def is_on(self):
+        return self.condition.get("pwr_on", False)
+
+    @property
+    def percentage(self):
+        return self.condition.get("fan_speed")
+
+    async def async_set_percentage(self, percentage):
+        await self.api.set_fan_speed(self._device_id, int(percentage))
+        await self.coordinator.async_request_refresh()
